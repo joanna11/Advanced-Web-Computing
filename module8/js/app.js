84e03a4663f9f6f231ab5d2cb9a46d9101355b83
joanna11 <jmmalunda@yahoo.com>
2013-09-14 15:55:19 +0800

    First Commit

---
+++ module8/js/app.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+
+    $('#searchForm').submit(function(){
+        webSearch();
+        return false;
+		
+    });
+	
+
+    function webSearch(){
+		
+		
+        var inp = $('#s').val();
+        var apiURL = 'http://api.rottentomatoes.com/api/public/v1.0/movies.json';
+        $.ajax({
+            type: "GET",
+            data: {
+                q: inp,
+                apiKey: 'yqmxyvyjaff6v6ufx9j4n3ru',
+            },
+            url: apiURL,
+            dataType:"jsonp",
+            success: showMovies
+        });
+    };
+	
+
+	
+function showMovies(response) { 
+        console.log('response', response);
+		var hitTemplate = Handlebars.compile($("#hit-template").html());
+        var movies = response.movies;
+		var rs = $("#rs");
+        rs.empty();
+		rs.html(""); 
+        for (var i = 0; i < movies.length; i++) {
+        var movie = movies[i];
+		rs.append(hitTemplate({title: movie.title, 
+									
+									poster: movie.posters.detailed,
+									year: movie.year
+									
+									
+									}));
+		
+		
+        };
+		
+		var hitTemplate2 = Handlebars.compile($("#founded-template").html());
+		var resulta = $("#resulta");
+		resulta.html("");
+		resulta.empty();
+		resulta.append(hitTemplate2({found: response.movies.length}));
+		  
+    }; 
+
+
+    
+});
